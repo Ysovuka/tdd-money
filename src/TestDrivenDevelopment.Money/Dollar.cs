921503465f5b38baea38cd8a715c88e7d7b0c895
Joshua Thompson <ysovuka@gmail.com>
2017-05-12 21:49:18 -0500

    Chapter 1

---
+++ src/TestDrivenDevelopment.Money/Dollar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestDrivenDevelopment.Money
+{
+    public class Dollar
+    {
+        public Dollar(int amount)
+        {
+            this.amount = amount;
+        }
+
+        public int amount = 0;
+
+        public void times(int multiplier)
+        {
+            amount *= multiplier;
+        }
+    }
+}
---
+++ tests/TestDrivenDevelopment.Money.Tests/Multiplication.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TestDrivenDevelopment.Money.Tests
+{
+    public class Multiplication
+    {
+        [Fact]
+        public void testMultiplication()
+        {
+            Dollar five = new Dollar(5);
+            five.times(2);
+            Assert.Equal(10, five.amount);
+        }
+    }
+}
